354647ee98929044d103b24492b5eb4c1f51ba11
edgarplor <45710328+edgarplor@users.noreply.github.com>
2020-10-07 12:34:31 -0500

    editing header_removal to fit the dataset

--- header_removal.sh
+++ header_removal.sh
@@ -1,6 +1,6 @@
 # Place in 16k-LP7 from TSPSpeech.iso and run to concatenate wave files
 # into one headerless training file
-for i in /data/dataset/mandarin_female/wavs/*.wav
+for i in /data/dataset/LJSpeech-1.1/wavs/*.wav
 do
-sox $i -r 16000 -c 1 -t sw - > mandarin_female/wavs/${i##*/}.s16 
+sox $i -r 16000 -c 1 -t sw - > LJSpeech-1.1/wavs/${i##*/}.s16 
 done
