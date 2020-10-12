a8aef3b8066a82dc50a2bfb6ae43493e8628df53
edgarplor <45710328+edgarplor@users.noreply.github.com>
2020-10-12 15:55:39 -0500

    dfsa

--- header_removal.sh
+++ header_removal.sh
@@ -4,3 +4,8 @@ for i in ./data/dataset/LJSpeech-1.1/wavs/*.wav
 do
 sox $i -r 16000 -c 1 -t sw - > LJSpeech-1.1/wavs/${i##*/}.s16 
 done
+
+for i in ./LJSpeech-1.1/wavs/*.s16;
+do 
+cat $i >> output.s16;
+done
