5a9c4a0eb94771b9aff4b8fb01b7e5d2dff71172
edgarplor <45710328+edgarplor@users.noreply.github.com>
2020-10-12 01:04:40 -0500

    afd

--- header_removal.sh
+++ header_removal.sh
@@ -2,5 +2,5 @@
 # into one headerless training file
 for i in ./data/dataset/LJSpeech-1.1/wavs/*.wav
 do
-sox $i -r 16000 -c 1 -t sw LJSpeech-1.1/wavs/${i##*/}.s16 
+sox $i -r 16000 -c 1 -t sw - > LJSpeech-1.1/wavs/${i##*/}.s16 
 done
